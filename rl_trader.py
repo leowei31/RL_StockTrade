b40dc76d8791cf45cc98b494305e57dbebf95a65
Leo <leo.wei@alumni.ubc.ca>
2021-03-25 23:13:50 -0600

    added libaries

---
+++ rl_trader.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.optimizers import Adam
+
+from datetime import datetime
+import itertools
+import argparse
+import re
+import os
+import pickle
+
+from sklearn.preprocessing import StandardScaler
