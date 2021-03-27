900067ba415008236dfc1b88ed36065a61efdca9
Leo <leo.wei@alumni.ubc.ca>
2021-03-26 21:23:46 -0600

    added data and environment

--- rl_trader.py
+++ rl_trader.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, Input
@@ -13,3 +14,82 @@ import os
 import pickle
 
 from sklearn.preprocessing import StandardScaler
+
+print("GPU is", "available" if tf.config.list_physical_devices("GPU") else "NOT AVAILABLE")
+
+from rl_stock_env import MultiStockEnv
+
+def get_data():
+    df = pd.read_csv('aapl_msi_sbux.csv')
+    return df.values
+
+
+# Circular Replay Buffer
+class ReplayBuffer:
+  def __init__(self, obs_dim, act_dim, size):
+    self.obs1_buf = np.zeros([size, obs_dim], dtype=np.float32)
+    self.obs2_buf = np.zeros([size, obs_dim], dtype=np.float32)
+    self.acts_buf = np.zeros(size, dtype=np.uint8)
+    self.rews_buf = np.zeros(size, dtype=np.float32)
+    self.done_buf = np.zeros(size, dtype=np.uint8)
+    self.ptr, self.size, self.max_size = 0, 0, size
+
+  def store(self, obs, act, rew, next_obs, done):
+    self.obs1_buf[self.ptr] = obs
+    self.obs2_buf[self.ptr] = next_obs
+    self.acts_buf[self.ptr] = act
+    self.rews_buf[self.ptr] = rew
+    self.done_buf[self.ptr] = done
+    self.ptr = (self.ptr+1) % self.max_size
+    # Note this is the circular pointer of the buffer
+    self.size = min(self.size+1, self.max_size)
+
+  def sample_batch(self, batch_size=32):
+    idxs = np.random.randint(0, self.size, size=batch_size)
+    return dict(s=self.obs1_buf[idxs],
+                s2=self.obs2_buf[idxs],
+                a=self.acts_buf[idxs],
+                r=self.rews_buf[idxs],
+                d=self.done_buf[idxs])
+
+def get_scaler(env):
+  # return scikit-learn scaler object to scale the states
+  # Note: you could also populate the replay buffer here
+
+  states = []
+  for _ in range(env.n_step):
+    action = np.random.choice(env.action_space)
+    state, reward, done, info = env.step(action)
+    states.append(state)
+    if done:
+      break
+
+  scaler = StandardScaler()
+  scaler.fit(states)
+  return scaler
+
+def maybe_make_dir(directory):
+  if not os.path.exists(directory):
+    os.makedirs(directory)
+
+def mlp(input_dim, n_action, n_hidden_layers=1, hidden_dim=32):
+  """ A multi-layer perceptron """
+
+  # input layer
+  i = Input(shape=(input_dim,))
+  x = i
+
+  # hidden layers
+  for _ in range(n_hidden_layers):
+    x = Dense(hidden_dim, activation='relu')(x)
+  
+  # final layer
+  x = Dense(n_action)(x)
+
+  # make the model
+  model = Model(i, x)
+
+  model.compile(loss='mse', optimizer='adam')
+  print((model.summary()))
+  return model
+
